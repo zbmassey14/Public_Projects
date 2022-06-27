d99c79b81a137c6612fd29f59d12dd7c76bcbb22
zbmassey14 <107360909+zbmassey14@users.noreply.github.com>
2022-06-26 21:12:21 -0500

    Update PRICE_FORECASTING_Keras_LSTM_LLY.py

--- Financial Projects/Python/PRICE_FORECASTING_Keras_LSTM_LLY.py
+++ Financial Projects/Python/PRICE_FORECASTING_Keras_LSTM_LLY.py
@@ -1,52 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
+#LLY Stock Price Forecasting with LSTMs
+#Zak Massey
 
-# In[113]:
+------------------------------------------------------------------------------------
+  
+  
+#This project used multiple online resources
+#Listed in the summary file
 
+#Import libraries
+....
 
-#Import packages
-import pandas as pd
-import matplotlib.pyplot as plt
-import fmpsdk
-import pyfmpcloud
-import numpy as np
-import ta as ta
-from pyfmpcloud import stock_time_series as sts
-from pyfmpcloud import settings
-from ta import add_all_ta_features
-from ta.momentum import RSIIndicator
-from ta.momentum import RSIIndicator
-from ta.utils import dropna
-from ta.volatility import BollingerBands
-from functools import reduce
-import seaborn as sns
-import os
-import datetime
-import IPython
-import IPython.display
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import tensorflow as tf
-import random
-
-#Adding technical indicators
-from ta.momentum import RSIIndicator
-from ta.momentum import AwesomeOscillatorIndicator
-from ta.momentum import ROCIndicator
-from ta.momentum import pvo
-from ta.trend import ADXIndicator
-from ta.others import DailyLogReturnIndicator
-from ta.trend import CCIIndicator
-from ta.volatility import AverageTrueRange
-from ta.momentum import KAMAIndicator
-from ta.trend import MassIndex
-from ta.trend import DPOIndicator
-
-
-# In[114]:
+#Adding technical indicators libraries
+...
 
 
 #Project Goal: Predict Stock Price Movement
@@ -58,27 +23,25 @@ from ta.trend import DPOIndicator
 #We then evaluate the performance & deploy the model for the next months predictions
 
 
-# In[115]:
+------------------------------------------------------------------------------------
 
 
 #Set the API key
-settings.set_apikey('39c5facda41abb1e5c6336fb376f40ed')
+settings.set_apikey('Your API Key')
 
 #Pull stock price data "lly"
-stock = sts.historical_stock_data('LLY', dailytype = 'change', start = '2015-04-15', end = '2022-06-20')
+stock = sts.historical_stock_data('LLY', dailytype = 'change', start = '2015-04-15',
+                                  end = '2022-06-20')
 #Need to reverse the order
 stock = stock[::-1] 
 stock.close
-
-
-# In[116]:
 
 
 #Look at a plot
 plt.plot(stock.close) #or plt.plot(lly["close"])
 
 
-# In[117]:
+------------------------------------------------------------------------------------
 
 
 #Adding technical indicators  to be used as feature inputs
@@ -157,7 +120,7 @@ dpo = dpo.dpo()
 dpo = dpo.to_frame()
 
 
-# In[118]:
+------------------------------------------------------------------------------------
 
 
 #Merge the indicators & stock price data
@@ -176,7 +139,7 @@ stock3
 #All of the features would be avaible for the inputs
 
 
-# In[119]:
+------------------------------------------------------------------------------------
 
 
 #Pre-processing:
@@ -200,7 +163,7 @@ test_df = stock3[int(n*0.9):]
 num_features = stock3.shape[1]
 
 
-# In[120]:
+------------------------------------------------------------------------------------
 
 
 #Normalizing the data for the NN
@@ -217,15 +180,11 @@ val_df = (val_df - train_mean) / train_std
 test_df = (test_df - train_mean) / train_std
 
 
-# In[121]:
-
-
 #Now the data is prepped for the tensorflow functions
 #https://www.tensorflow.org/tutorials/structured_data/time_series
+#sourced out the functions from another scrpit
 get_ipython().run_line_magic('run', 'tf_windowing.ipynb')
 
-
-# In[122]:
 
 
 #Tensorflow Functions:
@@ -235,16 +194,13 @@ get_ipython().run_line_magic('run', 'tf_windowing.ipynb')
 #These functions were developed by TensorFlow
 #The functoins are:
 #1. Windowing the data into TF datasets
-#2. Spliting the windows
-#3. Making TF dataset for the models
-#4. Plotting the outputs
-#5. Feedback loop for AR model
-#6. A compile & fit function for each model
+#2. A compile & fit function for each model
     #For this we changed the source code to allow for different optimizers, etc
+#3. Plotting the outputs
 
+------------------------------------------------------------------------------------
 
-# In[123]:
-
+#Notes on the functions:
 
 #Define the window using the Window Generator Function
 #The models will make a set of predictions based on a window of consecuitive samples
@@ -278,8 +234,7 @@ get_ipython().run_line_magic('run', 'tf_windowing.ipynb')
 #short term fluxuations
 
 
-# In[124]:
-
+#4. Plotting the outputs
 
 #SINGLE STEP Models:
 #Predicts ONE time step in the future based ONLY ON CURRENT CONDITIONS
@@ -289,13 +244,12 @@ get_ipython().run_line_magic('run', 'tf_windowing.ipynb')
 #are pretty useless.
 
 
-# In[125]:
+------------------------------------------------------------------------------------
 
 
-#LSTM Model1
+#LSTM Model1 (Single Step)
 #This will be a basic model that is trained on 30 days & returns ONE prediction in the future
 #Return_sequences=True allows the model to be trained on the 30 days at a time
-
 #Since this model only predicts ONE DAY in the future, it is not that useful
 #But this helps build a foundation of understanding
 
@@ -309,9 +263,7 @@ wide_window = WindowGenerator(
 wide_window
 wide_window.plot()
 
-
-# In[126]:
-
+------------------------------------------------------------------------------------
 
 ##LSTM Model1 Part 2: Defining the Model
 lstm_model1 = tf.keras.models.Sequential([
@@ -331,39 +283,9 @@ val_performance['LSTM1'] = lstm_model1.evaluate(wide_window.val)
 performance['LSTM1'] = lstm_model1.evaluate(wide_window.test, verbose=0)
 wide_window.plot(lstm_model1)
 
+#***See summary for output and explainations
 
-# In[127]:
-
-
-#We can see the predictions are very close to the labels. 
-#But,
-#This is for one time step at a time
-#Meaning only ONE FUTURE day was predicted at a time
-
-#So given the nature of the goal in mind, 
-#This is not very useful in gaining insight on the future movement of stock prices
-
-#Because of this, we will now move on to Muti-Step models, 
-#Which have much more value with respect to our goal of predicting future price movement
-
-
-# In[ ]:
-
-
-###########################################################################################################################
-###########################################################################################################################
-###########################################################################################################################
-###########################################################################################################################
-###########################################################################################################################
-###########################################################################################################################
-###########################################################################################################################
-###########################################################################################################################
-###########################################################################################################################
-###########################################################################################################################
-###########################################################################################################################
-
-
-# In[128]:
+------------------------------------------------------------------------------------
 
 
 #Multi step models
@@ -378,27 +300,13 @@ wide_window.plot(lstm_model1)
     #AutoRegressive - Models makes single step predictions which are fed back as input to the model    
 
 
-# In[129]:
-
-
-#Model inputs
-#These are bascially the same as defined above
-
-#Except in this case, the OUT_STEPS
-#Is applied to both the label_width & shift inputs
-#This is what make the model a single shot multistep model
-
-#input_width = The number of days to base the prediction off of
-#label_width/shift = Range/Sequence of days to predict in the future
-
-
-# In[132]:
+------------------------------------------------------------------------------------
 
 
 #Single Shot Multiple Step Predictions
 
 #SINGLE SHOT PREDICTIONS
-#THE 90 DAYS ARE USED ALL AT ONCE TO PREDICT THE NEXT 30
+#THE 120 DAYS ARE USED ALL AT ONCE TO PREDICT THE NEXT 30
 #In a "single shot",  the model will predict a future sequence
 
 MAX_EPOCHS = 50
@@ -412,14 +320,9 @@ multi_window.plot()
 multi_window
 
 
-# In[133]:
+------------------------------------------------------------------------------------
 
-
-#Now to develope a "baseline"
-#Which only repeats the previous's price
-
-#This is where the model literal just continously predicts the last value
-#Not very good, as you would expect
+#Baseline Model
 
 class MultiStepLastBaseline(tf.keras.Model):
   def call(self, inputs):
@@ -437,38 +340,20 @@ multi_performance['Baseline'] = last_baseline.evaluate(multi_window.test, verbos
 multi_window.plot(last_baseline)
 
 
-# In[134]:
-
-
-#As one would expect, the models are not very good
-#They do not do much learning, but this is used as a baseline
-#for the future predctions (mean_absolute_error: 3.4402)
-#Below we will start on the actual models that have usage
-
-
-# In[135]:
-
+------------------------------------------------------------------------------------
 
 #Single Shot Multi Step Model
-#Note:
-#Return_sequences is set to false because...
-#In a single shot format, the model onlt needs to produce an output  at the last timestep
-#For instance
-#inputs = 1:90
-#outputs = 91:120
-#No need to return the sequences to the model since it is only making ONE 30-day prediction
-#If return sequences was TRUE, then it would train on 1:90 -> make prediction, 2:91->make prediction & so on
 
-
-# In[136]:
-
+#The following is quite repetitive.
+#It is just the adhoc experimentation mentioned in the summary
+#Chaning parameters/adding layers etc.
 
 #Model 1
 multi_lstm_model1 = tf.keras.Sequential([
     tf.keras.layers.LSTM(units=256,
                          activation="tanh",
                          return_sequences=False),
-    tf.keras.layers.Dense(OUT_STEPS*num_features), #Default Dense Layer
+    tf.keras.layers.Dense(OUT_STEPS*num_features),
     tf.keras.layers.Reshape([OUT_STEPS, num_features])
 ])
 
@@ -482,22 +367,12 @@ multi_performance['LSTM_1'] = multi_lstm_model1.evaluate(multi_window.test, verb
 multi_window.plot(multi_lstm_model1)
 
 
-# In[137]:
-
-
-#The default model started out all over the place.
-#After adjusting the LSTM Layer Units, the predictions got much better
-#Started at 32 & work our way up to 256
-
-#The unit size
-
-
-# In[138]:
+------------------------------------------------------------------------------------
 
 
 multi_lstm_model2 = tf.keras.Sequential([
     tf.keras.layers.LSTM(units=256,
-                         activation="tanh", #Default, has to be TANH
+                         activation="tanh",
                          kernel_initializer="VarianceScaling",
                          recurrent_initializer="orthogonal",
                          return_sequences=False),
@@ -516,20 +391,12 @@ multi_performance['LSTM_2'] = multi_lstm_model2.evaluate(multi_window.test, verb
 multi_window.plot(multi_lstm_model2)
 
 
-# In[139]:
-
-
-#For this iteration we experimented with a few different parameters
-#We changed the initializers for the LSTM layer
-#Also learned that leaving the Dense Layer's activation function to "linear"(none) worked best
-
-
-# In[140]:
+------------------------------------------------------------------------------------
 
 
 multi_lstm_model3 = tf.keras.Sequential([
     tf.keras.layers.LSTM(units=256,
-                         activation="tanh", #Default, has to be TANH
+                         activation="tanh",
                          kernel_initializer="VarianceScaling",
                          recurrent_initializer="orthogonal",
                          return_sequences=False),
@@ -550,13 +417,11 @@ multi_val_performance['LSTM_3'] = multi_lstm_model3.evaluate(multi_window.val)
 multi_performance['LSTM_3'] = multi_lstm_model3.evaluate(multi_window.test, verbose=0)
 multi_window.plot(multi_lstm_model3)
 
-
-# In[141]:
-
+------------------------------------------------------------------------------------
 
 multi_lstm_model4 = tf.keras.Sequential([
     tf.keras.layers.LSTM(units=256,
-                         activation="tanh", #Default, has to be TANH
+                         activation="tanh",
                          kernel_initializer="VarianceScaling",
                          recurrent_initializer="orthogonal",
                          return_sequences=False),
@@ -578,12 +443,12 @@ multi_performance['LSTM_4'] = multi_lstm_model4.evaluate(multi_window.test, verb
 multi_window.plot(multi_lstm_model4)
 
 
-# In[142]:
+------------------------------------------------------------------------------------
 
 
 multi_lstm_model5 = tf.keras.Sequential([
     tf.keras.layers.LSTM(units=256,
-                         activation="tanh", #Default, has to be TANH
+                         activation="tanh",
                          kernel_initializer="VarianceScaling",
                          recurrent_initializer="TruncatedNormal",
                          return_sequences=False),
@@ -605,12 +470,12 @@ multi_performance['LSTM_5'] = multi_lstm_model5.evaluate(multi_window.test, verb
 multi_window.plot(multi_lstm_model5)
 
 
-# In[148]:
+------------------------------------------------------------------------------------
 
 
 multi_lstm_model6 = tf.keras.Sequential([
     tf.keras.layers.LSTM(units=256,
-                         activation="tanh", #Default, has to be TANH
+                         activation="tanh",
                          kernel_initializer="VarianceScaling",
                          recurrent_initializer="TruncatedNormal",
                          return_sequences=False),
@@ -633,13 +498,12 @@ multi_val_performance['LSTM_6'] = multi_lstm_model6.evaluate(multi_window.val)
 multi_performance['LSTM_6'] = multi_lstm_model6.evaluate(multi_window.test, verbose=0)
 multi_window.plot(multi_lstm_model6)
 
-
-# In[154]:
+------------------------------------------------------------------------------------
 
 
 multi_lstm_model7 = tf.keras.Sequential([
     tf.keras.layers.LSTM(units=256,
-                         activation="tanh", #Default, has to be TANH
+                         activation="tanh",
                          kernel_initializer="VarianceScaling",
                          recurrent_initializer="TruncatedNormal",
                          return_sequences=False),
@@ -663,8 +527,7 @@ multi_performance['LSTM_7'] = multi_lstm_model7.evaluate(multi_window.test, verb
 multi_window.plot(multi_lstm_model7)
 
 
-# In[155]:
-
+------------------------------------------------------------------------------------
 
 #Results:
 single_shot_results = pd.DataFrame(multi_val_performance)
@@ -672,29 +535,4 @@ single_shot_results = single_shot_results.transpose()
 single_shot_results.columns = ["Val Loss", "Val MAE"]
 single_shot_results
 
-
-# In[145]:
-
-
-#Results 2:
-
-
-# In[146]:
-
-
-#We can look at the MAE and see which model was comparitively the most accurate.
-#But we can also look at the plotted predictions to see how the models behaved.
-#This will allows us to make a more informed decision rather than basing our
-#approach off of the MAE
-
-
-# In[147]:
-
-
-#Tomorrow making closing remarks about which one is the best
-#And make a summary about lstm models and the varaible choices and thought processes
-#what is an lstm & how do they work - what are they used for
-#what is the structure of an lstm and how do the models flow - what are they made of
-#Which model is the best according to MAE, which model is the best according to you
-#which model looks like it would perform best considering the goal of predicting price movement?
 
